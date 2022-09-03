44e5385de7c87942c19cb3228106176bd4cbac4d
RobinHoo <53432684+robin0512@users.noreply.github.com>
2022-09-03 15:56:47 +0800

    create firebase data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from "react";
 import {AiOutlinePlus} from "react-icons/ai"
 import Todo from "./Todo";
 import {db} from "./firebase"
-import {query, collection, onSnapshot, updateDoc, doc} from "firebase/firestore"
+import {query, collection, onSnapshot, updateDoc, doc, addDoc} from "firebase/firestore"
+import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -15,22 +16,35 @@ const style = {
 }
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
 //Create todo
+const createTodo = async (e) => {
+  e.preventDefault(e);
+  if(input === ''){
+    alert('Please enter a valid todo')
+    return
+  }
+  await addDoc(collection(db, 'todos'),{
+    text: input,
+    completed: false,
+  })
+  setInput('')
+};
 
 //Read todo from firebase
 useEffect(()=>{
-  const q = query(collection(db, 'todos'))
+  const q = query(collection(db, 'todos'));
   const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    let todosArr = []
+    let todosArr = [];
     querySnapshot.forEach((doc) => {
-      todosArr.push({...doc.data(), id: doc.id})
+      todosArr.push({...doc.data(), id: doc.id});
     });
-    setTodos(todosArr)
+    setTodos(todosArr);
   })
-  return () => unsubscribe
-})
+  return () => unsubscribe()
+},[]);
 
 //Update todo in firebase
 const toggleComplete = async (todo) => {
@@ -44,8 +58,8 @@ const toggleComplete = async (todo) => {
     <div className={style.bg}>
       <div className={style.container}>
       <h3 className={style.heading}>Todo App</h3>
-      <form className={style.form}>
-        <input className={style.input} type="text" placeholder="Add Todo" />
+      <form onSubmit={createTodo} className={style.form}>
+        <input value={input} onChange={(e) => setInput(e.target.value)} className={style.input} type="text" placeholder="Add Todo" />
         <button className={style.button}><AiOutlinePlus size={30}/></button>
       </form>
       <ul> 
